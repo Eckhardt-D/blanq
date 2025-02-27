068e4fac1bd282ed49cbcfb5a8e5bbca81a8df4c
Eckhardt-D <eckhardt.dreyer@gmail.com>
2025-02-27 11:28:42 +0200

    chore: update app page

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu'
 </script>
 
 <template>
@@ -6,6 +7,35 @@
     <AppNavigation />
 
     <div class="flex-grow overflow-y-scroll container border-x">
+      <div class="p-5">
+        <UiNavigationMenu>
+          <UiNavigationMenuList>
+            <UiNavigationMenuItem>
+              <UiNavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+                <NuxtLink exact-active-class="bg-muted" to="/app">
+                  Dashboard
+                </NuxtLink>
+              </UiNavigationMenuLink>
+            </UiNavigationMenuItem>
+
+            <UiNavigationMenuItem>
+              <UiNavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+                <NuxtLink exact-active-class="bg-muted" to="/app/another-page">
+                  Another Page
+                </NuxtLink>
+              </UiNavigationMenuLink>
+            </UiNavigationMenuItem>
+
+            <UiNavigationMenuItem>
+              <UiNavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+                <NuxtLink exact-active-class="bg-muted" to="/app/third-page">
+                  Third Page
+                </NuxtLink>
+              </UiNavigationMenuLink>
+            </UiNavigationMenuItem>
+          </UiNavigationMenuList>
+        </UiNavigationMenu>
+      </div>
       <slot />
     </div>
   </main>
